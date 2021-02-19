cd49cc95af42a5a742249e6b838652eafa88cb3a
george <kesaev@gmail.com>
2021-02-19 20:26:19 +0200

    remove porter

--- porter_out/compile_c_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc svc_linear.c -std=c99 -lm -o svc_linear.o
-gcc svc_poly.c -std=c99 -lm -o svc_poly.o
-gcc svc_rbf.c -std=c99 -lm -o svc_rbf.o
-gcc svc_sigmoid.c -std=c99 -lm -o svc_sigmoid.o
--- porter_out/run_classification.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./svc_linear.o 1 2 3 4
