1d1d4c40c5de9f60a34ef9043045f88c88afa612
george <kesaev@gmail.com>
2021-02-20 18:06:16 +0200

    add read configuration from json file

--- src/client_read_parameters_encrypt_query.cpp
+++ src/client_read_parameters_encrypt_query.cpp
@@ -1,12 +1,38 @@
 #include "utils.h"
+#include <json.hpp>
+
 
 using namespace std;
 using namespace seal;
+using json = nlohmann::json;
 
-int main()
-// client code
+int main(int argc, char* argv[])
 {
-    ifstream parameters_file_stream("parameters.dat", ios::binary);
+	if (argc != 2)
+	{
+		cerr << "Provide path to configurations directory" << endl;
+		cout << "/bin/client_read_parameters_encrypt_query <path to config directory>" << endl;
+		cout << "For example:" << endl;
+		cout << "\"${HOMOMORPHIC_ROOT}\"/bin/client_read_parameters_encrypt_query \"${HOMOMORPHIC_ROOT}\"/example_inputs" << endl;
+
+		return -1;
+	}
+
+	auto config_dir_path = string(argv[1]);
+
+	std::ifstream i(config_dir_path + "/client_query.json");
+	json input_json;
+	i >> input_json;
+
+	auto user_query = input_json["query"].get<std::vector<double>>();
+	auto x = input_json["scale"]["x"].get<int>();
+	auto y = input_json["scale"]["y"].get<int>();
+
+	cout << "Client read cliend query" << endl;
+	cout << "\tquery: " << input_json["query"] << endl;
+	cout << "\tscale.x: " << x << " scale.y: " << y << endl;
+
+    ifstream parameters_file_stream(config_dir_path + "/parameters.dat", ios::binary);
     EncryptionParameters params;
     params.load(parameters_file_stream);
 
@@ -16,15 +42,15 @@ int main()
     auto public_key = key_generator.public_key();
     auto secret_key = key_generator.secret_key();
 
-    ofstream ofs_secret_key("secret_key.dat", ios::binary);
+    ofstream ofs_secret_key(config_dir_path + "/secret_key.dat", ios::binary);
     secret_key.save(ofs_secret_key);
 
     auto relin_keys = key_generator.relin_keys_local();         // TODO: switch to non local
     auto galois_keys = key_generator.galois_keys_local();
 
-    ofstream ifs_relin("relin.dat", ios::binary);
-    ofstream ifs_galois("galois.dat", ios::binary);
-    ofstream ifs_public_key("public_key.dat", ios::binary);
+    ofstream ifs_relin(config_dir_path + "/relin.dat", ios::binary);
+    ofstream ifs_galois(config_dir_path + "/galois.dat", ios::binary);
+    ofstream ifs_public_key(config_dir_path + "/public_key.dat", ios::binary);
 
     auto rlk_size = relin_keys.save(ifs_relin);
     auto gal_size = galois_keys.save(ifs_galois);
@@ -39,17 +65,17 @@ int main()
     size_t slot_count = ckks_encoder.slot_count();
     cout << "number of slots: " << slot_count << ", should be half of the size of poly modulus degree" << endl;
 
-    vector<double> user_query{0.25, -0.25};
     cout << "Input vector:" << endl;
     print_vector(user_query);
 
     Plaintext user_query_plain_text;
-    double scale = pow(2, 40);
+
+    double scale = pow(x, y);
     print_line(__LINE__);
     ckks_encoder.encode(user_query, scale, user_query_plain_text);
 
     Ciphertext encrypted_user_query;
     encryptor.encrypt(user_query_plain_text, encrypted_user_query);
-    ofstream ofs_encrypted_query("encrypted_query.dat", ios::binary);
+    ofstream ofs_encrypted_query(config_dir_path + "/encrypted_query.dat", ios::binary);
     encrypted_user_query.save(ofs_encrypted_query);
 }
