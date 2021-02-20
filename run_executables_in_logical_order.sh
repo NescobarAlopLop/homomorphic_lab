87a5c4344fff6948b2a9be8d0d13be7f6f433270
george <kesaev@gmail.com>
2021-02-20 17:54:49 +0200

    add read configuration from json file

--- run_executables_in_logical_order.sh
+++ run_executables_in_logical_order.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+export HOMOMORPHIC_ROOT
 HOMOMORPHIC_ROOT=$(pwd)
 
+
+echo "Compiling executables..."
 mkdir -p "${HOMOMORPHIC_ROOT}"/build && cd "${HOMOMORPHIC_ROOT}"/build || exit
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j 4
 cd "${HOMOMORPHIC_ROOT}" || exit
+echo "Finished compilation."
+
+echo -e "\n 1. Server: generating parameters..."
+"${HOMOMORPHIC_ROOT}"/bin/server_generate_parameters "${HOMOMORPHIC_ROOT}"/example_inputs
 
 bin/server_generate_parameters
 bin/client_read_parameters_encrypt_query
--- src/server_generate_parameters.cpp
+++ src/server_generate_parameters.cpp
@@ -1,20 +1,43 @@
 #include "utils.h"
+#include <json.hpp>
+
 
 using namespace std;
 using namespace seal;
+using json = nlohmann::json;
 
-int main()
+int main(int argc, char* argv[])
 {
-    ofstream ofs_parameters("parameters.dat", ios::binary);
+	if (argc != 2)
+	{
+		cerr << "Provide path to configurations directory" << endl;
+		cout << "/bin/server_generate_parameters <path to config directory>" << endl;
+		cout << "For example:" << endl;
+		cout << "\"${HOMOMORPHIC_ROOT}\"/bin/server_generate_parameters \"${HOMOMORPHIC_ROOT}\"/example_inputs" << endl;
+
+		return -1;
+	}
+	auto config_dir_path = string(argv[1]);
+
+	cout << config_dir_path + "/encryption_parameters_config.json" << endl;
+	std::ifstream i(config_dir_path + "/encryption_parameters_config.json");
+	json input_json;
+	i >> input_json;
+
+	std::size_t poly_modulus_degree = input_json["poly_modulus_degree"].get<std::size_t>();
+	auto bit_sizes = input_json["bit_sizes"].get<std::vector<int>>();
+	cout << "Server has read the following encryption parameters configurations:" << endl;
+	cout << "\tpoly_modulus_degree: " << input_json["poly_modulus_degree"] << endl;
+	cout << "\tbit_sizes: " << endl;
+	for (const auto& value: bit_sizes)
+		std::cout << value << ' ';
 
     EncryptionParameters encryption_parameters(scheme_type::CKKS);
-    std::size_t poly_modulus_degree = 8192;
     encryption_parameters.set_poly_modulus_degree(poly_modulus_degree);
-//        auto bit_sizes = {40, 40, 40, 40, 40}; // 84 mb
-    auto bit_sizes = {40, 40, 40, 40}; // 53 mb
     auto coefficient_modulus_degree = CoeffModulus::Create(poly_modulus_degree, bit_sizes);
     encryption_parameters.set_coeff_modulus(coefficient_modulus_degree);
 
-    auto parameters_size = encryption_parameters.save(ofs_parameters);
-    cout << "Server: saved encryption parameters: wrote " << parameters_size << " bytes" << endl;
+	ofstream ofs_parameters(config_dir_path + "/parameters.dat", ios::binary);
+	auto parameters_size = encryption_parameters.save(ofs_parameters);
+    cout << endl << "Server: saved encryption parameters: wrote " << parameters_size << " bytes" << endl;
 }
